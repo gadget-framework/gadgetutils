02dad83e43466a6acec2bd4ae1d38554002a0472
will <will.butler@hafogvatn.is>
2022-03-22 11:37:55 +0000

    readwrite consistent naming

--- R/g3_readwrite.R
+++ R/g3_readwrite.R
@@ -1,24 +1,25 @@
 #' @export
-write.tmb.param <- function(params, dir, file.name){
+write.g3.param <- function(params, gd, file_name){
   
   params$value <- unlist(params$value)
   params$optimise <- as.numeric(params$optimise)
   params$random <- as.numeric(params$random)
+  params[params$type == '', 'type'] <- '.'
   
-  write.g3.file(params, dir, file.name)
+  write.g3.file(params, gd, file_name)
   
 }
 
 #' @export
-write.g3.file <- function(obj, dir, file_name){
+write.g3.file <- function(obj, gd, file_name){
   
   if (tibble::is_tibble(obj)) obj <- as.data.frame(obj)
   
-  outfile <- file(file.path(dir, file_name), 'w')
+  outfile <- file(file.path(gd, file_name), 'w')
   utils::capture.output(print(format(obj, 
                                      scientific = FALSE, 
                                      drop0trailing = TRUE), 
-                              path = dir, 
+                              path = gd, 
                               row.names = FALSE,
                               max = 999999), 
                         file = outfile)
