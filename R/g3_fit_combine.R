3b0e24d83816c7da2b61abea10dc581a22873017
will <will.butler@hafogvatn.is>
2025-05-30 11:48:21 +0000

    bind_fit exported and documented

--- R/g3_fit_combine.R
+++ R/g3_fit_combine.R
@@ -24,8 +24,20 @@ bind_fit_components <- function(fit_list, component){
   
 }
 
-#' this is a possible generalisation to bind all the elements of multiple gadget.fit objects
-bind_fit <- function(fit_list, ...){
+#' @title Combine multiple gadget.fit objects
+#' @description This function combines multiple gadget.fit objects into a single fit object.
+#' An additional column 'id' is added to each element of the resulting list to identify 
+#' the individual gadget.fits
+#' @param fit_list A list of multiple gadget.fit objects. 
+#' @details
+#' The 'id' column will be taken from names(fit_list). If fit_list has no names, the 'id'
+#' column will be taken from the positions, e.g., 
+#' fit_list = list(a = fit1, b = fit2) will be produces id's of 'a' and 'b', whereas 
+#' fit_list = list(fit1, fit2) will produce id's of '1' and '2'
+#' 
+#' @returns A gadget.fit object
+#' @export
+bind_fit <- function(fit_list){
   # initialise an empty list
   out <- fit_list[[1]]
   out[] <- NA
@@ -33,7 +45,7 @@ bind_fit <- function(fit_list, ...){
     if(sum(class(fit_list[[1]][[names(out)[i]]]) == "data.frame") == 1){
       tmp <- sapply(fit_list,function(x){x[names(out)[i]]})
       names(tmp) <- names(fit_list) # replace with readable model names
-      out[[i]] <- bind_rows(tmp, .id="id")
+      out[[i]] <- dplyr::bind_rows(tmp, .id="id")
     } else { NULL }}
 
   return(out)
